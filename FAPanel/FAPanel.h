8d132b0f9a8cf4877ec444960de2fbee5bf4329a
Fahid Attique <fahadattique@appsgenii.eu>
2017-08-04 22:16:22 +0500

    Working on carthage support

---
+++ FAPanel/FAPanel.h
@@ -0,0 +1,19 @@
+//
+//  FAPanel.h
+//  FAPanel
+//
+//  Created by Fahid Attique on 04/08/2017.
+//  Copyright © 2017 Fahid Attique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FAPanel.
+FOUNDATION_EXPORT double FAPanelVersionNumber;
+
+//! Project version string for FAPanel.
+FOUNDATION_EXPORT const unsigned char FAPanelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FAPanel/PublicHeader.h>
+
+
